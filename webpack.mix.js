a2090ab18c973e48ea856506419c3265e2b70633
iamgergo <hello@iamgergo.com>
2020-09-14 18:59:23 +0200

    dev fixes

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,5 +4,9 @@ mix.setPublicPath('./public')
     .js('resources/js/app.js', 'app.js')
     .sass('resources/sass/app.scss', 'app.css')
     .options({ processCssUrls: false })
-    .sourceMaps()
-    .version();
+    .sourceMaps();
+
+// Symlinking...
+// ln -s /.../packages/thepinecode/bazar/public/app.js /.../public/vendor/bazar/app.js
+// ln -s /.../packages/thepinecode/bazar/public/app.css /.../public/vendor/bazar/app.css
+// ln -s /.../packages/thepinecode/bazar/resources/img /.../public/vendor/bazar/
