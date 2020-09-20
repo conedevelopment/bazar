1c4e70ab9b7913e4d49331690fc43e8f10d8b406
iamgergo <hello@iamgergo.com>
2020-09-20 09:11:02 +0200

    cleanup jobs

--- tests/Feature/CommandsTest.php
+++ tests/Feature/CommandsTest.php
@@ -2,20 +2,13 @@
 
 namespace Bazar\Tests\Feature;
 
-use Bazar\Jobs\ClearCarts;
-use Bazar\Jobs\ClearChunks;
 use Bazar\Tests\TestCase;
-use Illuminate\Support\Facades\Queue;
 
 class CommandsTest extends TestCase
 {
     /** @test */
     public function it_can_clear_carts()
     {
-        Queue::fake();
-        ClearCarts::dispatch();
-        Queue::assertPushed(ClearCarts::class);
-
         $this->artisan('bazar:clear-carts', ['--all' => true])
             ->expectsOutput('All carts have been deleted.')
             ->assertExitCode(0);
@@ -28,10 +21,6 @@ class CommandsTest extends TestCase
     /** @test */
     public function it_can_clear_chunks()
     {
-        Queue::fake();
-        ClearChunks::dispatch();
-        Queue::assertPushed(ClearChunks::class);
-
         $this->artisan('bazar:clear-chunks')
             ->expectsOutput('File chunks are cleared!')
             ->assertExitCode(0);
