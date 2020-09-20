85855cc3ff9ce41f56ff1c5a19c37b23785000de
iamgergo <hello@iamgergo.com>
2020-09-20 16:14:15 +0200

    transaction tests

--- tests/Unit/TransactionTest.php
+++ tests/Unit/TransactionTest.php
@@ -4,6 +4,7 @@ namespace Bazar\Tests\Unit;
 
 use Bazar\Database\Factories\OrderFactory;
 use Bazar\Database\Factories\TransactionFactory;
+use Bazar\Support\Facades\Gateway;
 use Bazar\Tests\TestCase;
 
 class TransactionTest extends TestCase
@@ -19,10 +20,66 @@ class TransactionTest extends TestCase
     }
 
     /** @test */
-    public function a_transaction_belongs_to_an_order()
+    public function it_belongs_to_an_order()
     {
         $this->assertTrue(
             $this->order->transactions->pluck('id')->contains($this->transaction->id)
+        );
+    }
+
+    /** @test */
+    public function it_has_driver_name()
+    {
+        $this->assertSame(Gateway::driver($this->transaction->driver)->name(), $this->transaction->driverName);
+        $this->assertSame('fake', $this->transaction->driver('fake')->driverName);
+    }
+
+    /** @test */
+    public function it_has_url()
+    {
+        $this->assertSame(
+            Gateway::driver($this->transaction->driver)->transactionUrl($this->transaction),
+            $this->transaction->url
+        );
+
+        $this->assertNull($this->transaction->driver('fake')->url);
+    }
+
+    /** @test */
+    public function it_can_be_completed()
+    {
+        $this->transaction->markAsPending();
+        $this->assertNull($this->transaction->completed_at);
+        $this->assertTrue($this->transaction->pending());
+        $this->assertFalse($this->transaction->completed());
+
+        $this->transaction->markAsCompleted();
+        $this->assertNotNull($this->transaction->completed_at);
+        $this->assertTrue($this->transaction->completed());
+        $this->assertFalse($this->transaction->pending());
+    }
+
+    /** @test */
+    public function it_has_query_scopes()
+    {
+        $this->assertSame(
+            $this->transaction->newQuery()->payments()->toSql(),
+            $this->transaction->newQuery()->where('type', 'payment')->toSql(),
+        );
+
+        $this->assertSame(
+            $this->transaction->newQuery()->refunds()->toSql(),
+            $this->transaction->newQuery()->where('type', 'refund')->toSql(),
+        );
+
+        $this->assertSame(
+            $this->transaction->newQuery()->completed()->toSql(),
+            $this->transaction->newQuery()->whereNotNull('completed_at')->toSql(),
+        );
+
+        $this->assertSame(
+            $this->transaction->newQuery()->pending()->toSql(),
+            $this->transaction->newQuery()->whereNull('completed_at')->toSql(),
         );
     }
 }
