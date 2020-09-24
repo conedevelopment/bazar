cd5f4fb34fc7df7844be549afbb4cf2966f62d2f
iamgergo <hello@iamgergo.com>
2020-09-24 15:38:37 +0200

    cleanup base controller

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -3,11 +3,9 @@
 namespace Bazar\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests;
 }
