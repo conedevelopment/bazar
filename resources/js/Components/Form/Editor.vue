84a6c40412e6613f058f50aa1a00f20a6e764df6
iamgergo <hello@iamgergo.com>
2020-10-24 17:19:04 +0200

    prepare image handling

--- resources/js/Components/Form/Editor.vue
+++ resources/js/Components/Form/Editor.vue
@@ -14,9 +14,8 @@
 
         mounted() {
             this.editor = new Quill(this.$refs.input, this.config);
-
             this.editor.root.innerHTML = this.value;
-            // this.editor.enable(! this.disabled);
+            this.editor.enable(! this.attrs.disabled);
             this.editor.on('text-change', () => this.update());
         },
 
@@ -30,12 +29,18 @@
             config() {
                 return {
                     modules: {
-                        toolbar: [
-                            [{ header: [1, 2, 3, 4, false] }],
-                            ['bold', 'italic', 'underline'],
-                            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-                            ['clean']
-                        ]
+                        toolbar: {
+                            container: [
+                                [{ header: [1, 2, 3, 4, false] }],
+                                ['bold', 'italic', 'underline'],
+                                [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                                ['clean'],
+                                // ['image']
+                            ],
+                            // handlers: {
+                            //     image: this.mediaHandler
+                            // }
+                        }
                     },
                     theme: 'snow',
                     formats: ['header', 'bold', 'underline', 'italic', 'list'],
@@ -47,7 +52,25 @@
         methods: {
             update() {
                 this.$emit('input', this.editor.root.innerHTML === '<p><br></p>' ? '' : this.editor.root.innerHTML);
-            }
+            },
+            // mediaHandler() {
+            //     this.$refs.media.open();
+            // },
+            // insertMedia(values) {
+            //     const range = this.editor.getSelection() || 0;
+            //     values.forEach(value => {
+            //         if (value.is_image) {
+            //             this.editor.insertEmbed(
+            //                 range ? range.index : 0, 'image', value.urls.original, Quill.sources.USER
+            //             );
+            //         } else {
+            //             this.editor.insertText(
+            //                 range ? range.index : 0, value.name, 'link', value.urls.original, Quill.sources.USER
+            //             );
+            //         }
+            //     });
+            //     this.$refs.media.selection = [];
+            // }
         }
     }
 </script>
@@ -55,13 +78,10 @@
 <template>
     <div class="form-group">
         <label v-if="label" :for="name">{{ label }}</label>
-        <div
-            ref="input"
-            class="editor form-control"
-            :class="{ 'is-invalid': invalid }"
-        ></div>
+        <div ref="input" class="editor form-control" :class="{ 'is-invalid': invalid }"></div>
         <span v-if="help || invalid" class="form-text" :class="{ 'text-danger': invalid }">
             {{ error || help }}
         </span>
+        <!-- <media-manager ref="media" multiple @input="insertMedia"></media-manager> -->
     </div>
 </template>
