0d8562e905e697fbb0d360b58e16fbe712c4f7ed
iamgergo <hello@iamgergo.com>
2020-10-26 19:32:52 +0100

    don't use mix by default

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,21 +7,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     {{-- Icons --}}
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('vendor/bazar/img/favicon/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('vendor/bazar/img/favicon/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('vendor/bazar/img/favicon/favicon-16x16.png') }}">
-    <link rel="manifest" href="{{ asset('vendor/bazar/img/favicon/site.webmanifest') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset('vendor/bazar/img/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ URL::asset('vendor/bazar/img/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ URL::asset('vendor/bazar/img/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ URL::asset('vendor/bazar/img/favicon/site.webmanifest') }}">
 
     {{-- Styles --}}
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600&display=swap" rel="stylesheet">
-    <link href="{{ asset(mix('vendor/bazar/app.css')) }}" rel="stylesheet">
+    <link href="{{ URL::asset('vendor/bazar/app.css') }}" rel="stylesheet">
 
     {{-- Scripts --}}
     <script>
         window.initialPage = {!! json_encode($page) !!};
         window.translations = {!! json_encode($translations) !!};
     </script>
-    <script src="{{ asset(mix('vendor/bazar/app.js')) }}" defer></script>
+    <script src="{{ URL::asset('vendor/bazar/app.js') }}" defer></script>
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             Bazar.boot();
