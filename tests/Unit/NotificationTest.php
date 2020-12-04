4b0bdf18fc9d2bce64e9bb3d5cf48bcbe6914c95
iamgergo <hello@iamgergo.com>
2020-12-04 09:21:00 +0100

    tests

---
+++ tests/Unit/NotificationTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Bazar\Tests\Unit;
+
+use Bazar\Database\Factories\OrderFactory;
+use Bazar\Notifications\AdminNewOrder;
+use Bazar\Notifications\CustomerNewOrder;
+use Bazar\Tests\TestCase;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\URL;
+
+class NotificationTest extends TestCase
+{
+    /** @test */
+    public function admin_order_notification_can_be_sent_on_different_channels()
+    {
+        $order = OrderFactory::new()->create();
+        $notification = new AdminNewOrder($order);
+
+        $this->assertInstanceOf(MailMessage::class, $notification->toMail($this->admin));
+        $this->assertSame([
+            'message' => __('A new order has been placed.'),
+            'url' => URL::route('bazar.orders.show', $order),
+        ], $notification->toArray($this->admin));
+    }
+
+    /** @test */
+    public function customer_order_notification_can_be_sent_on_different_channels()
+    {
+        $order = OrderFactory::new()->create();
+        $notification = new CustomerNewOrder($order);
+
+        $this->assertInstanceOf(MailMessage::class, $notification->toMail($this->user));
+        $this->assertSame([], $notification->toArray($this->user));
+    }
+}
