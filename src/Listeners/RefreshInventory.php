76e1d31ab9236d8bca192d7753e477f0548c66d9
Viktor Szépe <viktor@szepe.net>
2020-12-13 10:56:11 +0100

    Add more precise type in RefreshInventory

--- src/Listeners/RefreshInventory.php
+++ src/Listeners/RefreshInventory.php
@@ -3,6 +3,7 @@
 namespace Bazar\Listeners;
 
 use Bazar\Contracts\Models\Product;
+use Bazar\Contracts\Stockable;
 use Bazar\Events\OrderPlaced;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
@@ -29,7 +30,7 @@ class RefreshInventory
             }
 
             return $shouldReject;
-        })->groupBy(function (Model $product): string {
+        })->groupBy(function (Stockable $product): string {
             return get_class($product).':'.$product->id;
         })->each(function (Collection $products): void {
             tap($products->first(), static function (Model $product) use ($products): void {
