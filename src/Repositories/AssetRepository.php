363aa3bc614eb610df8974a48a186f4a61cc6745
iamgergo <hello@iamgergo.com>
2021-01-24 12:17:39 +0100

    Prepare asset repository

---
+++ src/Repositories/AssetRepository.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Bazar\Repositories;
+
+class AssetRepository extends Repository
+{
+    /**
+     * Register a new asset.
+     *
+     * @param  string  $name
+     * @param  string  $path
+     * @param  string  $type
+     * @return void
+     */
+    public function register(string $name, string $path, string $type): void
+    {
+        $this->items->put("{$name}-{$type}", $path);
+    }
+
+    /**
+     * Register a new script.
+     *
+     * @param  string  $name
+     * @param  string  $path
+     * @return void
+     */
+    public function script(string $name, string $path): void
+    {
+        $this->register($name, $path, 'script');
+    }
+
+    /**
+     * Register a new style.
+     *
+     * @param  string  $name
+     * @param  string  $path
+     * @return void
+     */
+    public function style(string $name, string $path): void
+    {
+        $this->register($name, $path, 'script');
+    }
+
+    /**
+     * Get all the registerd scripts.
+     *
+     * @return array
+     */
+    public function scripts(): array
+    {
+        return $this->items->filter(static function (string $path, string $name): bool {
+            return preg_match('/script$/', $name);
+        })->toArray();
+    }
+
+    /**
+     * Get all the registered styles.
+     *
+     * @return array
+     */
+    public function styles(): array
+    {
+        return $this->items->filter(static function (string $path, string $name): bool {
+            return preg_match('/style$/', $name);
+        })->toArray();
+    }
+}
---
+++ tests/Unit/AssetRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bazar\Tests\Unit;
+
+use Bazar\Tests\TestCase;
+
+class AssetRepositoryTest extends TestCase
+{
+    /** @test */
+    public function it_can_register_scripts()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function it_can_register_styles()
+    {
+        $this->assertTrue(true);
+    }
+}
