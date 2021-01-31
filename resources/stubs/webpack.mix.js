5e63ab980ef8f44b716ac3dff293833b7d0660d5
iamgergo <hello@iamgergo.com>
2021-01-31 15:13:16 +0100

    upgrade to mix v6

--- resources/stubs/webpack.mix.js
+++ resources/stubs/webpack.mix.js
@@ -1,4 +1,5 @@
 mix.js('resources/js/vendor/bazar/app.js', 'public/vendor/bazar')
+    .vue({ version: 2 })
     .sass('resources/sass/vendor/bazar/app.scss', 'public/vendor/bazar')
     .options({ processCssUrls: false })
     .sourceMaps()
