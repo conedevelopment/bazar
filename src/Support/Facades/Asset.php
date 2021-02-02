3b3fb4cb93d58ab0666747a60744f4985c20bf4b
iamgergo <hello@iamgergo.com>
2021-02-02 22:40:10 +0100

    docblocks

--- src/Support/Facades/Asset.php
+++ src/Support/Facades/Asset.php
@@ -6,12 +6,15 @@ use Bazar\Contracts\Repositories\AssetRepository;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static void register(string $name, string $path, sting $type)
- * @method static void script(string $name, string $path)
- * @method static void style(string $name, string $path)
+ * @method static void register(string $name, string $source, string $type, array $options)
+ * @method static void script(string $name, string $source, array $options)
+ * @method static void style(string $name, string $source, array $options)
+ * @method static void icon(string $name, string $source, array $options)
  * @method static void remove(string $name)
  * @method static array scripts()
  * @method static array styles()
+ * @method static array icons()
+ * @method static void link()
  *
  * @see \Bazar\Contracts\Repositories\AssetRepository
  */
