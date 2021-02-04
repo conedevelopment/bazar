94ef43d7548571986306d46ed7fdf067225ac3e9
Viktor Szépe <viktor@szepe.net>
2021-02-04 18:39:02 +0100

    crop and resize methods are not present in Image
    but in GdImage

--- src/BazarServiceProvider.php
+++ src/BazarServiceProvider.php
@@ -209,11 +209,11 @@ class BazarServiceProvider extends ServiceProvider
      */
     protected function registerConversions(): void
     {
-        Support\Facades\Conversion::register('thumb', static function (Conversion\Image $image): void {
+        Support\Facades\Conversion::register('thumb', static function (Conversion\GdImage $image): void {
             $image->crop(500, 500);
         });
 
-        Support\Facades\Conversion::register('medium', static function (Conversion\Image $image): void {
+        Support\Facades\Conversion::register('medium', static function (Conversion\GdImage $image): void {
             $image->resize(1400, 1000);
         });
     }
