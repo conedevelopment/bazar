5f0dd00b5807bda3ff4f43351d23f01c2e980397
iamgergo <hello@iamgergo.com>
2021-03-26 08:23:53 +0100

    test

--- tests/Unit/EventTest.php
+++ tests/Unit/EventTest.php
@@ -12,9 +12,9 @@ class EventTest extends TestCase
     /** @test */
     public function it_clears_cookies_after_logout()
     {
-        Cookie::queue('cart_token', 'fake', 864000);
+        Cookie::queue('cart_id', 'fake', 864000);
 
-        $this->assertSame('cart_token', Cookie::getQueuedCookies()[0]->getName());
+        $this->assertSame('cart_id', Cookie::getQueuedCookies()[0]->getName());
         $this->assertTrue(time() < Cookie::getQueuedCookies()[0]->getExpiresTime());
 
         Event::dispatch(new Logout('web', $this->user));
