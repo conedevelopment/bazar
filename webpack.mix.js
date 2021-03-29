56f4c520627562cfdac2b4c56f959ebdee6f4373
iamgergo <hello@iamgergo.com>
2021-03-29 11:03:48 +0200

    replace npm to yarn

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,21 +3,21 @@ const mix = require('laravel-mix');
 mix.webpackConfig({
     output: {
         publicPath: '/vendor/bazar/',
-        chunkFilename: '[name].js'
+        chunkFilename: '[name].js',
     },
     resolve: {
         alias: {
-            'vue$': 'vue/dist/vue.runtime.esm.js'
-        }
+            'vue$': 'vue/dist/vue.runtime.esm.js',
+        },
     },
     externals: {
-        moment: 'moment'
-    }
+        moment: 'moment',
+    },
 });
 
 mix.setPublicPath('./public')
     .js('resources/js/app.js', 'app.js')
-    .vue({ version: 2 })
+    .vue({ version: 3, runtimeOnly: true })
     .sass('resources/sass/app.scss', 'app.css')
     .options({ processCssUrls: false })
     .sourceMaps()
