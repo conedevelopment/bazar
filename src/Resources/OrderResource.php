54e00d2a6f1af37a5771bcdfd64b0ada98f8a053
D. Nagy Gergő <hello@iamgergo.com>
2022-01-24 16:02:44 +0100

    wip

--- src/Resources/OrderResource.php
+++ src/Resources/OrderResource.php
@@ -2,12 +2,24 @@
 
 namespace Cone\Bazar\Resources;
 
+use Cone\Root\Fields\Date;
 use Cone\Root\Fields\ID;
+use Cone\Root\Fields\Text;
 use Cone\Root\Resources\Resource;
 use Illuminate\Http\Request;
 
 class OrderResource extends Resource
 {
+    /**
+     * The relations to eager load on every query.
+     *
+     * @var array
+     */
+    protected array $with = [
+        'items',
+        'shipping',
+    ];
+
     /**
      * Define the fields for the resource.
      *
@@ -18,6 +30,8 @@ class OrderResource extends Resource
     {
         return [
             ID::make(),
+            Text::make(__('Total'), 'formatted_total')->visibleOnIndex(),
+            Date::make(__('Created at'), 'created_at')->visibleOnIndex(),
         ];
     }
 }
