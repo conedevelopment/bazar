9ebf833bbd642d0fd882f65fb3ff14aca108f434
D. Nagy Gergő <hello@iamgergo.com>
2024-03-19 12:10:45 +0100

    wip

--- src/Widgets/OrdersCount.php
+++ src/Widgets/OrdersCount.php
@@ -2,7 +2,9 @@
 
 namespace Cone\Bazar\Widgets;
 
+use App\Models\Order;
 use Cone\Root\Widgets\Value;
+use Illuminate\Database\Eloquent\Builder;
 
 class OrdersCount extends Value
 {
@@ -10,4 +12,12 @@ class OrdersCount extends Value
      * The widget icon.
      */
     protected ?string $icon = 'archive';
+
+    /**
+     * Create a new Eloquent query.
+     */
+    public function query(): Builder
+    {
+        return Order::query()->status(Order::FULFILLED);
+    }
 }
